faa3932b75f9e9f012a5a27d316112a9c7e140a7
Danny Rosado Castro <drosadocastro@gmail.com>
2026-01-11 12:54:05 -0400

    Update __init__.py

--- core/retrieval/__init__.py
+++ core/retrieval/__init__.py
@@ -1,3 +1,8 @@
 """
-Retrieval logic including vector, lexical, and hybrid pipelines.
+Retrieval package exposing hybrid/vector retrieval helpers.
 """
+
+from . import retrieval_engine
+from .retrieval_engine import *  # noqa: F401,F403
+
+__all__ = retrieval_engine.__all__
