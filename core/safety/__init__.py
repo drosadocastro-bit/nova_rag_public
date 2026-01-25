96b5929a9620328abb7b4e201c1949bcebf0173b
Danny Rosado Castro <drosadocastro@gmail.com>
2026-01-24 21:40:54 -0400

    Update __init__.py

--- core/safety/__init__.py
+++ core/safety/__init__.py
@@ -4,9 +4,12 @@ Safety and risk assessment utilities.
 
 from .risk_assessment import RiskAssessment, RiskLevel
 from .injection_handler import handle_injection_and_multi_query
+from .anomaly_detector import AnomalyDetector, AnomalyResult
 
 __all__ = [
 	"RiskAssessment",
 	"RiskLevel",
 	"handle_injection_and_multi_query",
+	"AnomalyDetector",
+	"AnomalyResult",
 ]
